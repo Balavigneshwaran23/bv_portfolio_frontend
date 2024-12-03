2515f4139674a2e56efe4784fb82ab28237a7108
BALAVIGNESHWARN.P <137701826+BALAVIGNESHWARAN23@users.noreply.github.com>
2024-12-03 20:41:10 +0530

    update

--- src/Components/hero.js
+++ src/Components/hero.js
@@ -1,4 +1,4 @@
-import bala from "D:/githubreptasks/bv_portfolio/src/assets/images/hero-removebgmain.png";
+import bala from "D:/githubreptasks/bv_portfolio/src/assets/images/hero-removebg.png";
 import { ReactComponent as SvgCurve } from "D:/githubreptasks/bv_portfolio/src/assets/svg/SVG curve.svg";
 import { useEffect, useRef } from "react";
 import React from 'react';
